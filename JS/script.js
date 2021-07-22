bd361adf1379409b76d83068c6a53cbc8ee46c80
vusechec <vusechec@ibero.edu.co>
2021-07-21 20:22:01 -0500

    Actividad4

---
+++ JS/script.js
@@ -0,0 +1,101 @@
+
+$('.primera').css('color','blue')
+$('.size').mouseover(function(){
+    $(this).css('font-size','40px')
+
+
+   })
+ 
+
+$('.segunda').click(function(){
+    $('.segunda').css('background','pink');
+    $('.segunda').dblclick(function(){
+        $(this).css('background','rgb(199, 199, 224)')
+        })
+
+   })
+
+$('.tercera').dblclick(function(){
+    $('.tercera').css('font-size', '30px')
+
+})   
+$('.mouse').mouseover(function(){
+    $(this).css('background','red')
+})
+
+$('.mouse').mouseout(function(){
+    $(this).css('background','rgb(199, 199, 224)')
+})
+
+
+$('.cuarta').click(function(){
+    $(this).css('color','green')
+})
+
+
+
+$('.quinta').click(function(){
+    $(this).css('background','purple')
+})
+
+$('.letra').dblclick(function(){
+ $(this).css('font-family','cursive')
+})
+
+$('.sexta').mouseover(function(){
+    $(this).css('font-size','60px')
+})
+
+$('.lupa').mouseout(function(){
+    $(this).css('font-size','15px')})
+
+$('.septima').mouseover(function(){
+    $(this).css('background','brown')
+})
+
+$('.septima').mouseout(function(){
+    $(this).css('background','rgb(199, 199, 224)')
+})
+
+$('.octava').mouseover(function(){
+    $(this).css('font-size','25px')})
+
+
+    $('.novena').click(function(){
+        $('.novena').css('background','black');
+        $('.novena').dblclick(function(){
+            $(this).css('color','white')
+            })
+
+       })
+
+$('.decima').click(function(){
+    $(this).css('font-size','30px')
+})
+
+
+
+
+$('.box').mouseover(function(){
+    $(this).css('background','orange')
+})
+
+$('.box').mouseout(function(){
+    $(this).css('background','lightcoral')
+})
+
+$('.box2').mouseover(function(){
+    $(this).css('background','blue')
+})
+
+$('.box2').mouseout(function(){
+    $(this).css('background','lightcoral')
+})
+
+$('.box3').mouseover(function(){
+    $(this).css('background','yellow')
+})
+
+$('.box3').mouseout(function(){
+    $(this).css('background','rgb(153, 92, 161)')
+})
